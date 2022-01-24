29e138e4acd63e7feef2b27c834e60b03d593a96
dlwnstn0524 <ljs052450@gmail.com>
2022-01-24 14:54:44 +0900

    220124

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,14 @@
+package hello.core.discount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    void discount() {
+    }
+}
